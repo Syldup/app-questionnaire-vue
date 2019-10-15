30d29b2117061795ed2bd0e097300943e44d88f1
Syldup <syd9up@gmail.com>
2019-10-15 13:31:13 +0200

    Afficher composent formRegister et formQuestion

---
+++ src/components/formQuestion.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="form-question">
+    <b-form @submit="onSubmit">
+      <b-form-group :label="question" label-size="lg">
+        <b-form-radio-group
+          v-model="selected"
+          :options="answers1"
+          size="lg" buttons stacked
+          button-variant="outline-info"
+          name="radio-btn-stacked">
+        </b-form-radio-group>
+        <b-form-radio-group v-if="answers2"
+          v-model="selected"
+          :options="answers2"
+          size="lg" buttons stacked
+          button-variant="outline-info"
+          name="radio-btn-stacked">
+        </b-form-radio-group>
+      </b-form-group>
+
+      <b-button type="Commencer le test" variant="primary">Submit</b-button>
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'form-question',
+  props: ['question', 'answers'],
+  data () {
+    return {
+      selected: 0,
+      answers1: this.answers,
+      answers2: []
+    }
+  },
+  methods: {
+    onSubmit (evt) {
+      // evt.preventDefault()
+      // alert(JSON.stringify(this.form))
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue App"/> -->
+    Hi
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+  name: 'home'
+  // components: {
+  //   HelloWorld
+  // }
 }
 </script>
